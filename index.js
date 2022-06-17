c5905d8e0d212cc9033599e0695900ce4a0af147
bill apiyo <billian177@gmail.com>
2022-06-17 19:27:18 +0300

    commit tree 29319b3c7b7451801827b62ece67779106797811

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const values = {};
+
+  for (const val of array) {
+    // n steps
+    const complement = target - val;
+    if (complement in values) return true;
+    values[val] = true;
+  }
+
+  return false;
 }
+
+
+/*function hasTargetSum(array, target) {
+  
+  // Write your algorithm here
+  
+    for (let i=0; i< array.length; i++) {
+      
+      const remain = Math.abs(target - array[i]);
+      if (array.includes(remain)){
+       return true;
+      } else{
+        return false;
+      }
+    }
+}*/
 
 /* 
   Write the Big O time complexity of your function here
+  O(1)
 */
 
 /* 
   Add your pseudocode here
+  Write a function hasTargetSum
+  It takes two parameters; an array and target number
+    Sum the values of each pair in the array
+      compare the pair-total to the target number
+        return true if atleast one pair returns value equal to target 
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
